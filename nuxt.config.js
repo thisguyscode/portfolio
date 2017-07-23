03319e14f7754a9a5427a8554d9298a0bc0b199a
jamie <jamie@socialcops.com>
2017-07-23 02:19:55 +0100

    Hard coded .wf classes and attributes into head for initial page-load

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ module.exports = {
     ],
     htmlAttrs: [
       { class: 'wf-loading' }
+    ],
+    style: [
+      { cssText: '.wf-loading { opacity: 0; visibility: hidden } .wf-active { opacity: 1; visibility: visible; transition: opacity .7s ease }', type: 'text/css' }
     ]
   },
   /*

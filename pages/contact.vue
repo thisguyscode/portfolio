c990264c06cbc09abddab258ce44f07c5639f82a
jamie <jamie@socialcops.com>
2017-08-05 16:10:38 +0100

    Added enformed form to contact.vue

---
+++ pages/contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="contact">
+    <form action="https://www.enformed.io/dyuof206" method="POST" >
+      <label for="name">Name</label>
+      <input type="text" name="name">
+      <label for="replyto">Email</label>
+      <input type="email" name="replyto">
+      <input type="submit" value="Send">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
